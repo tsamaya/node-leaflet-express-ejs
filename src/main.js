8153f5436e0e3a0c002d393c7f129625723b74c4
Martyn Eggleton <martyn.eggleton@gmail.com>
2018-04-25 06:33:28 +0100

    Getting colours from DSM and DTM WMS server tiles. height calculations that i have seem to be a different DEM format.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,36 @@ var LatLon = require("mt-latlon");
 var jQuery = require('jquery');
 var proxy = require('express-http-proxy');
 var morgan = require('morgan')
-
-
 var GeometryUtil = require("leaflet-geometryutil");
+
+L.TileLayer.ColorPickerWMS = L.TileLayer.WMS.extend({
+  options: {
+    crossOrigin: "anonymous"
+  },
+  getColor: function(latlng) {
+    var size = this.getTileSize();
+    var point = this._map.project(latlng, this._tileZoom).floor();
+    var coords = point.unscaleBy(size).floor();
+    var offset = point.subtract(coords.scaleBy(size));
+    coords.z = this._tileZoom;
+    var tile = this._tiles[this._tileCoordsToKey(coords)];
+    if (!tile || !tile.loaded) return null;
+    try {
+      var canvas = document.createElement("canvas");
+      canvas.width = 1;
+      canvas.height = 1;
+      var context = canvas.getContext('2d');
+      context.drawImage(tile.el, -offset.x, -offset.y, size.x, size.y);
+      return context.getImageData(0, 0, 1, 1).data;
+    } catch (e) {
+      return null;
+    }
+  }
+});
+L.tileLayer.colorPickerWMS = function(url, options) {
+  return new L.TileLayer.ColorPickerWMS(url, options);
+};
+
 
 function getDirectionalLine(target, angle, distance) {
   // get position of the sun (azimuth and altitude) at today's sunrise
@@ -35,6 +62,94 @@ var polyline = L.polyline(aLine, {
   color: 'red'
 }).addTo(map);
 
+
+
+
+console.log("times.sunrise", times.sunrise, "sunrisePos", sunrisePos, "sunriseAngle", sunriseAngle);
+
+
+dtmOptions = {
+  "layers":"LIDAR-DTM-TSR-2M-ENGLAND-EA-WMS",
+  "format":"image/png"
+}
+var dtmLayer = L.tileLayer.colorPickerWMS('http://environment.data.gov.uk/ds/wms?SERVICE=WMS&INTERFACE=ENVIRONMENT&LC=4400000000000000000000000000000&', dtmOptions).addTo(map);
+
+
+
+dsmOptions = {
+  "layers":"LIDAR-DSM-TSR-2M-ENGLAND-EA-WMS",
+  "format":"image/png"
+
+}
+var dsmLayer = L.tileLayer.colorPickerWMS('http://environment.data.gov.uk/ds/wms?SERVICE=WMS&INTERFACE=ENVIRONMENT&LC=4400000000000000000000000000000&', dsmOptions).addTo(map);
+
+function isFullOn(element, index, array) {
+  return element == 255;
+}
+
+aWhite = new Uint8Array(4);
+aWhite.set( [ 255, 255, 255, 255 ]);
+
+function isWhite(array){
+  return false;
+  aFull = array.filter(isFullOn)
+  return 4 == aFull.length
+}
+
+function getHeightAtPoint(point, dsm, dtm, bDSM){
+
+  var a = null;
+  if(bDSM){
+    a = dsm.getColor(point);
+    if (isWhite(a)){
+      a = null;
+    }
+  }
+  if (a == null){
+    a = dtm.getColor(point);
+    if (isWhite(a)){
+      a = null;
+    }
+  }
+
+  var h = NaN;
+  if (a !== null)
+    h = -10000 + (((a[0] * 256 * 256) + (a[1] * 256 )+ a[2]) * 0.001);
+  return h;
+}
+
+
+/*function getHeightAtPoint(point, dsm, dtm, bDSM){
+  aWhite = new Uint8Array(4);
+  aWhite.set( [ 255, 255, 255, 255 ]);
+  var a = null;
+  if(bDSM){
+    a = dsm.getColor(point);
+    if (isWhite(a)){
+      a = null;
+    }
+  }
+  if (a == null){
+    a = dtm.getColor(point);
+    if (isWhite(a)){
+      a = null;
+    }
+  }
+
+  var h = NaN;
+  if (a !== null)
+    h = -10000 + (((a[0] * 256 * 256) + (a[1] * 256 )+ a[2]) * 0.1);
+  return h;
+}
+
+*/
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
 function getPointsOnLine(map, aLine, steps){
   var aList = [];
   for (i=0; i<=10; i++){
@@ -47,7 +162,18 @@ function getPointsOnLine(map, aLine, steps){
 }
 
 var aPoints =  getPointsOnLine(map, aLine, 10);
-jsondata = {"locations":aPoints.map(function(point){
+
+function getHeights(){
+  console.log("aPoints",aPoints);
+  heights = [];
+  for (i=0; i<aPoints.length; i++){
+    heights.push(getHeightAtPoint(new L.LatLng(aPoints[i].latLng.lat, aPoints[i].latLng.lng) ,dsmLayer, dtmLayer, true));
+  }
+  console.log("heights",heights);
+}
+setTimeout(getHeights, 10000);
+
+/*jsondata = {"locations":aPoints.map(function(point){
   console.log('point', point);
   return {
           "latitude": point.latLng.lat,
@@ -70,15 +196,7 @@ jQuery.ajax({
     console.error(err);
   }
 });
-
-
-console.log("times.sunrise", times.sunrise, "sunrisePos", sunrisePos, "sunriseAngle", sunriseAngle);
-
-var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
+*/
 
 
 /*
